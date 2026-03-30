833a2974139b563f947ce91e4352a0a28ab532e3
janasakthip2005@gmail.com <your-email@example.com>
2026-03-30 20:55:04 +0530

    connect frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://hostel-backend-2fw3.onrender.com/api"
 });
 
 export const setAuthToken = (token) => {
